627cae22cbaa337424f5f0082ed55074f6c8ab7d
Elinsz <85796934+Elinsz@users.noreply.github.com>
2024-02-21 21:22:40 -0300

    commit

---
+++ src/SKUI/js/login.js
@@ -0,0 +1,23 @@
+function performLogin() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'check_login.php', true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var response = JSON.parse(xhr.responseText);
+            if (response.success) {
+                // Redirecionar para a página index ou fazer o que for necessário
+                window.location.href = 'elinsz.html';
+            } else {
+                alert('Login falhou. Verifique suas credenciais.');
+            }
+        }
+    };
+
+    var data = 'username=' + encodeURIComponent(username) + '&password=' + encodeURIComponent(password);
+    xhr.send(data);
+}
